ebfa3ac8266c30985569b3bf60ea3759c1f6ef8f
Татьяна Шульга <schulga.tatyana@yandex.ru>
2023-06-13 18:00:08 +0500

    fix_django_app

--- expert_system/expert_system/config/settings.py
+++ expert_system/expert_system/config/settings.py
@@ -1,18 +1,17 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+dotenv = load_dotenv(BASE_DIR.parent / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
+'''SECRET_KEY = (
     "django-insecure-k6xfs4y!2=k9a_=0a&y#_&$c*_)%he*!wpc1#drgf+@#-$hs=x"
-)
-
+)'''
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
