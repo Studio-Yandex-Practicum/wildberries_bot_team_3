34610be42fc46acb556b1d567ee19bdeace3e067
Юрий Достовалов <JDoss86@yandex.ru>
2023-06-14 01:01:04 +0300

    Structur folders

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1,7 +1,22 @@
-from telegram.ext import Application, CommandHandler
-from config import bot_token
 import logging
-from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+from telegram import InlineKeyboardMarkup
+from telegram.ext import (Application, CallbackQueryHandler, CommandHandler,
+                          MessageHandler, PicklePersistence, filters)
+
+from constants.messages import (ACCEPTANCE_RATE_ANSWER_MESSAGE,
+                                ACCEPTANCE_RATE_MESSAGE, ERROR_MESSAGE,
+                                HELLO_MESSAGE, LEFTOVERS_PARSER_MESSAGE,
+                                LEFTOVERS_PARSER_RESULT_MESSAGE,
+                                POSITION_PARSER_EXPECTATION_MESSAGE,
+                                POSITION_PARSER_MESSAGE,
+                                POSITION_PARSER_RESULT_MESSAGE,
+                                SUBSCRIPTIONS_MESSAGE)
+from constants.parser_constants import STOCS
+from keyboards import (leftovers_keyboard_input, main_keyboard, menu_keyboard,
+                       parsing_keyboard_expectation, parsing_keyboard_input,
+                       parsing_subscription_keyboard)
+from settings import bot_token
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,31 +25,153 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+async def check_callback(update, context):
+    """Функция-обработчик callback запросов"""
+    data = update.callback_query.data
+    if data == 'main_menu':
+        await start(update, context)
+    if data == 'position_parser':
+        await position_parser_info(update, context)
+    elif data == 'remainder_parser':
+        await remainder_parser_info(update, context)
+    elif data == 'acceptance_rate':
+        await acceptance_rate_info(update, context)
+    elif data == 'position_subscriptions':
+        await get_subscriptions(update, context)
+    elif data == 'another_parsing_request':
+        await position_parser_info(update, context)
+
+
 async def start(update, context):
     """Функция-обработчик для команды /start"""
-    start_message = (
-        "Привет! Чтобы воспользоваться ботом, нужно подписаться на наш "
-        "telegram канал https://t.me/mpexperts"
+    chat = update.effective_chat
+    await context.bot.send_message(
+        chat.id,
+        HELLO_MESSAGE,
+        reply_markup=InlineKeyboardMarkup(main_keyboard)
     )
-    ReplyKeyboardMarkup(
-        [[KeyboardButton('/start')]],
-        one_time_keyboard=True
-    )
+
+
+async def position_parser_info(update, context):
+    """Функция-обработчик для кнопки Парсер позиций"""
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=start_message,
-        reply_markup=ReplyKeyboardRemove(),
+        text=POSITION_PARSER_MESSAGE,
+        reply_markup=InlineKeyboardMarkup(parsing_keyboard_input)
+    )
+
+
+async def position_parser_wildberrys(update, context):
+    """Функция-вызов парсера (позиция в поиске)"""
+    result = 33
+    await position_parser_expectations(update, context)
+    await position_parser_result(update, context, result)
+
+
+async def position_parser_expectations(update, context):
+    """Функция-вызов текста ожидания и кнопки Отправить еще запрос"""
+    text_split = update.message.text.split()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=POSITION_PARSER_EXPECTATION_MESSAGE.format(
+            text_split[0], ' '.join(text_split[1:])
+        ),
+        reply_markup=InlineKeyboardMarkup(parsing_keyboard_expectation),
+        parse_mode='Markdown'
+    )
+
+
+async def position_parser_result(update, context, result):
+    """Функция-вывод результата и кнопки Подписки(1/6/12ч) и Возврата в меню"""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=POSITION_PARSER_RESULT_MESSAGE.format(result),
+        reply_markup=InlineKeyboardMarkup(parsing_subscription_keyboard)
+    )
+
+
+async def remainder_parser_info(update, context):
+    """Функция-обработчик для кнопки Парсер остатков"""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=LEFTOVERS_PARSER_MESSAGE,
+        reply_markup=InlineKeyboardMarkup(leftovers_keyboard_input)
+    )
+
+
+async def remainder_parser_wildberrys(update, context):
+    """Функция-вызов парсера по артикулу (остатки по складам и размерам)"""
+    # Артикул товара для парсера articul = update.message.text
+    result = []
+    await remainder_parser_result(update, context, result)
+
+
+async def remainder_parser_result(update, context, result):
+    """Функция-вывода результатов парсера по артикулу"""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=LEFTOVERS_PARSER_RESULT_MESSAGE.format(result),
+        reply_markup=InlineKeyboardMarkup(menu_keyboard)
+    )
+
+
+async def acceptance_rate_info(update, context):
+    """Функция-обработчик для кнопки Отслеживание коэффицианта приемки WB"""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=ACCEPTANCE_RATE_MESSAGE,
+    )
+
+
+async def acceptance_rate_answer(update, context):
+    """Функция-вывод результата Отслеживание коэффицианта приемки WB"""
+    text = update.message.text
+    if text in STOCS:
+        result = 33
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=ACCEPTANCE_RATE_ANSWER_MESSAGE.format(text, result),
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=ERROR_MESSAGE,
+            reply_markup=InlineKeyboardMarkup(menu_keyboard)
+        )
+
+
+async def get_subscriptions(update, context):
+    """Функция-обработчик для кнопки Мои подписки на позиции"""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=SUBSCRIPTIONS_MESSAGE,
+        reply_markup=InlineKeyboardMarkup(menu_keyboard)
     )
 
 
 def main():
-    """Создаём в директории bot файл config.py и прописываем туда """
-    """bot_token = "ВАШ_ТОКЕН_ОТ_БОТА" """
-    token = bot_token
-    bot = Application.builder().token(token).build()
-    logger.info("Бот успешно запущен.")
-    bot.add_handler(CommandHandler("start", start))
-    bot.run_polling()
+    persistence = PicklePersistence(filepath='callbackdatabot')
+    application = (
+        Application.builder()
+        .token(bot_token)
+        .persistence(persistence)
+        .arbitrary_callback_data(True)
+        .build()
+    )
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CallbackQueryHandler(check_callback))
+    application.add_handler(
+        MessageHandler(filters.Regex(r'^\d+$'), remainder_parser_wildberrys)
+    )
+    application.add_handler(
+        MessageHandler(
+            filters.Regex(r'^\d+(\s\w*)*'), position_parser_wildberrys
+        )
+    )
+    application.add_handler(
+        MessageHandler(filters.TEXT, acceptance_rate_answer)
+    )
+    application.run_polling()
 
 
 if __name__ == '__main__':
