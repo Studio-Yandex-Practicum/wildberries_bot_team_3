c71026ea9bcd8cdcc3db4a6fa78ce7095dc3239c
rabcrin <rabcrin@gmail.com>
2023-06-13 20:24:17 +0300

    set_bot_descr and show_stock added

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1,11 +1,12 @@
 from telegram.ext import (
-    Application, CommandHandler, ChatMemberHandler,
+    Application, CommandHandler, MessageHandler, filters,
 )
 from config import bot_token
 import logging
 from telegram import (
     ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove,
 )
+import requests
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,11 +15,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def welcome_new_members(update, context):
-    """Приветственное сообщение для новых участников чата"""
-    logger.info("welcome_new_members() успешно запущен.")
-    welcome_message = (
-        "Что умеет этот бот?\n\nПривет! На связи команда @...\n"
+def set_bot_description():
+    description = (
+        "Привет! На связи команда @...\n"
         "Мы создали этого бота для помощи всем действующим поставщикам "
         "Wildberries.\n\n"
         "Здесь вы можете:\n"
@@ -27,10 +26,16 @@ async def welcome_new_members(update, context):
         "- узнать остатки товара по размерам.\n\n"
         "Для начала работы нажмите Start"
     )
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=welcome_message,
-    )
+    method = 'setMyDescription'
+    url = f'https://api.telegram.org/bot{bot_token}/{method}'
+    data = {
+        "description": description
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        logger.info("Описание успешно установлено")
+    else:
+        logger.info("Ошибка при установке описания")
 
 
 async def start(update, context):
@@ -50,18 +55,56 @@ async def start(update, context):
     )
 
 
+async def show_stock(update, context):
+    """Функция-обработчик для команды /stock"""
+    message = (
+        "Отправьте артикул для вывода остатков:\n\n"
+        "Например:\n"
+        "36704403"
+    )
+    keyboard = ReplyKeyboardMarkup(
+        [[KeyboardButton('Отмена')]],
+        one_time_keyboard=True,
+        resize_keyboard=True,
+    )
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=message,
+        reply_markup=keyboard
+    )
+
+
+async def handle_cancel(update, context):
+    """Функция-обработчик для нажатия на кнопку 'Отмена'"""
+    message_text = update.message.text
+    if message_text == 'Отмена':
+        cancel_message = 'Действие отменено'
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=cancel_message
+        )
+    else:
+        user_input_message = f"Вы ввели: {message_text}"
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=user_input_message
+        )
+
+
 def main():
     """Создаём в директории bot файл config.py и прописываем туда """
     """bot_token = "ВАШ_ТОКЕН_ОТ_БОТА" """
     token = bot_token
+
+    set_bot_description()
+
     bot = Application.builder().token(token).build()
     logger.info("Бот успешно запущен.")
 
-    bot.add_handler(ChatMemberHandler(
-        welcome_new_members, ChatMemberHandler.CHAT_MEMBER
-    ))
-
     bot.add_handler(CommandHandler("start", start))
+
+    bot.add_handler(CommandHandler("stock", show_stock))
+    bot.add_handler(MessageHandler(filters.TEXT, handle_cancel))
 
     bot.run_polling()
 
