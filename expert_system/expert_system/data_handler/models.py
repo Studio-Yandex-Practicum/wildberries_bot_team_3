97d16bbaf596ac1c4214fed26c4f28b2c223ab46
rabcrin <rabcrin@gmail.com>
2023-06-16 16:11:35 +0300

    poetry pyproject and models fixed

--- expert_system/expert_system/data_handler/models.py
+++ expert_system/expert_system/data_handler/models.py
@@ -7,7 +7,7 @@ class Text(models.Model):
     text_header = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     text = HTMLField()
-    buttons = models.ManyToManyField('Button', related_name='text')
+    buttons = models.ManyToManyField('Button', related_name='text', blank=True)
 
     def __str__(self):
         return self.text_header
