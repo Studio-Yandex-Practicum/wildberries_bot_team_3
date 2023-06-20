d7c959cffb8c40f426a2723e2e74ca3ca9f6c65d
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-06-20 23:20:53 +0300

    fix stock callback def

--- bot/handlers/stock.py
+++ bot/handlers/stock.py
@@ -2,24 +2,20 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import CommandHandler
 
 
-async def show_stock(update, context):
-    """Функция-обработчик для команды /stock"""
-    message = (
-        "Отправьте артикул для вывода остатков:\n\n"
-        "Например:\n"
-        "36704403"
-    )
-    keyboard = ReplyKeyboardMarkup(
-        [[KeyboardButton('Отмена')]],
-        one_time_keyboard=True,
-        resize_keyboard=True,
-    )
+from keyboards import menu_keyboard
+from constants.messages import LEFTOVERS_PARSER_RESULT_MESSAGE
+from services.services import remainder_parser
+
+
+async def stock_callback(update, context):
+    """Функция-вывода результатов парсинга по артикулу"""
+    result = await remainder_parser(update)
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=message,
-        reply_markup=keyboard
+        text=LEFTOVERS_PARSER_RESULT_MESSAGE.format(result),
+        reply_markup=InlineKeyboardMarkup(menu_keyboard)
     )
 
 
 def stock_handlers(app):
-    app.add_handler(CommandHandler("stock", show_stock))
+    app.add_handler(CommandHandler("stock", stock_callback))
