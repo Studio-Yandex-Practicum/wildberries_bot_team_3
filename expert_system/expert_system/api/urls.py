a93a251299d32e27262af334833b61b25ef80eb8
Александр Лыо <al.lyo@yandex.ru>
2023-06-28 16:31:47 +0300

    fix

--- expert_system/expert_system/api/urls.py
+++ expert_system/expert_system/api/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 
-from expert_system.data_handler.views import ButtonViewSet, TextViewSet
-from expert_system.data_handler.views import (RequestPositionViewSet,
-                                              RequestRateViewSet,
-                                              RequestStockViewSet)
+from expert_system.data_handler.views import (
+    RequestPositionViewSet,
+    RequestRateViewSet,
+    RequestStockViewSet
+)
 
 
 app_name = "api"
 
 urlpatterns = [
-    path("button/<slug:slug>/", ButtonViewSet.as_view(), name="buttons"),
-    path("text/<slug:slug>/", TextViewSet.as_view(), name="texts"),
     path("request_position/", RequestPositionViewSet.as_view(),
          name="new_request_position"),
     path("request_stock/", RequestStockViewSet.as_view(),
--- expert_system/expert_system/data_handler/models.py
+++ expert_system/expert_system/data_handler/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from tinymce.models import HTMLField
 
 
 class Request(models.Model):
@@ -13,31 +12,6 @@ class Request(models.Model):
 
     class Meta:
         abstract = True
-
-
-class Text(models.Model):
-    id = models.AutoField(primary_key=True)
-    text_header = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
-    text = HTMLField()
-    buttons = models.ManyToManyField('Button', related_name='text', blank=True)
-
-    def __str__(self):
-        return self.text_header
-
-
-class Button(models.Model):
-    id = models.AutoField(primary_key=True)
-    cover_text = models.CharField(max_length=255)
-    slug = models.ForeignKey(
-        Text,
-        on_delete=models.SET_NULL,
-        related_name='button',
-        null=True
-    )
-
-    def __str__(self):
-        return self.cover_text
 
 
 class TelegramUser(models.Model):
