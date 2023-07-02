213bfad6b03326922fde3689348bb2ae811030e6
K1N88 <m-end-eleev@yandex.ru>
2023-07-02 12:00:42 +0300

    add conv handler stock

--- bot/constants/callback_data.py
+++ bot/constants/callback_data.py
@@ -1,6 +1,7 @@
 CHECK_SUBSCRIPTION = "check_start_subscription"
 MENU = "main_menu"
 CANCEL = "cancel"
+CANCEL_STOCK = "stock_cancel"
 GET_POSITION = "position_parser"
 GET_POSITION_SUBSCRIPTIONS = "position_subscriptions"
 GET_STOCK = "remainder_parser"
