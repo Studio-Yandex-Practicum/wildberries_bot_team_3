a2b27dcba2412bb639c97d14e6b173e49f4515d7
K1N88 <m-end-eleev@yandex.ru>
2023-07-05 23:14:19 +0300

    add docker2

--- expert_system/docker-entrypoint.sh
+++ expert_system/docker-entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
@@ -5,9 +7,8 @@ done
 
 >&2 echo "Postgres is up - executing command"
 
-poetry run python manage.py makemigrations
-poetry run python manage.py migrate
-poetry run python manage.py createsuperuser --noinput
-poetry run python manage.py runserver
+poetry run python expert_system/manage.py migrate
+poetry run python expert_system/manage.py createsuperuser --noinput
+poetry run python expert_system/manage.py runserver
 
 exec "$@"
