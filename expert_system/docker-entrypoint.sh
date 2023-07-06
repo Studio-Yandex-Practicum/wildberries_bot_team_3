a0b2ba412d250de3293baa264d1b9ca878b5f28a
K1N88 <m-end-eleev@yandex.ru>
2023-07-06 03:06:06 +0300

    add docker 3

--- expert_system/docker-entrypoint.sh
+++ expert_system/docker-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-
+poetry run python expert_system/manage.py makemigrations
 poetry run python expert_system/manage.py migrate
 poetry run python expert_system/manage.py createsuperuser --noinput
 poetry run python expert_system/manage.py runserver
--- expert_system/expert_system/config/settings.py
+++ expert_system/expert_system/config/settings.py
@@ -71,11 +71,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-        "NAME": os.getenv("POSTGRES_DB", default="postgres"),
-        "USER": os.getenv("POSTGRES_USER", default="postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-        "HOST": os.getenv("DB_HOST", default="db"),
-        "PORT": os.getenv("DB_PORT", default="5432"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
         "URL": os.getenv("DATABASE_URL"),
     }
 }
